0e50ebf18474c8ce21c1cf6369a93b5f45f05dac
Sudhanva Kalghatgi <sudhanvakalghatgi3@gmail.com>
2026-02-17 16:21:37 +0530

    Final Commit

---
+++ client/.vite/deps/react-hook-form.js
@@ -0,0 +1,36 @@
+import {
+  Controller,
+  Form,
+  FormProvider,
+  FormStateSubscribe,
+  Watch,
+  appendErrors,
+  createFormControl,
+  get,
+  set,
+  useController,
+  useFieldArray,
+  useForm,
+  useFormContext,
+  useFormState,
+  useWatch
+} from "./chunk-LLWO4OW2.js";
+import "./chunk-3IFX6TK2.js";
+import "./chunk-G3PMV62Z.js";
+export {
+  Controller,
+  Form,
+  FormProvider,
+  FormStateSubscribe,
+  Watch,
+  appendErrors,
+  createFormControl,
+  get,
+  set,
+  useController,
+  useFieldArray,
+  useForm,
+  useFormContext,
+  useFormState,
+  useWatch
+};
